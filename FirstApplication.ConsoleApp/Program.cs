bd532a2a5c635b9dd7e2ad61b4bf98a2591fd896
aventus <mix79@ya.ru>
2023-02-06 10:43:24 +0300

    Закоменчен запрос даты рождения, так как спрашиваем возраст. Использованы преобразования, как явные так и нет. Модуль 3.7

--- FirstApplication.ConsoleApp/Program.cs
+++ FirstApplication.ConsoleApp/Program.cs
@@ -7,20 +7,26 @@ namespace FirstApplication.ConsoleApp // Объявление пространс
     {
         static void Main(string[] args) // Объявление метода Main
         {
-            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance); //Решение проблемы с отображением кирилицы 
             var enc1251 = Encoding.GetEncoding(1251);
-
-            System.Console.OutputEncoding = System.Text.Encoding.UTF8; //Для вывода юникода в консоли, иначе ничего не выводит.
+            System.Console.OutputEncoding = System.Text.Encoding.UTF8; 
             System.Console.InputEncoding = enc1251;
-            Console.WriteLine("Как Вас зовут?"); // Задать вопрос
-            var name = Console.ReadLine(); // Запрос имени, запись в переменную name
-            Console.WriteLine($"Здравствуйте, {name}, сколько Вам лет?");
-            int age = Convert.ToInt32(Console.ReadLine()); //Запрос возраста, запись в переменную age
-            Console.WriteLine($"{name}, Какой у Вас размер обуви?");
-            double MyShoeSize = Convert.ToDouble(Console.ReadLine()); // Запрос имени, запись в переменную name      
+
+            // Запрос имени, запись в переменную name
+                        Console.WriteLine("Как Вас зовут?"); 
+            var name = Console.ReadLine();
+            //Запрос возраста, запись в переменную age. Конвертация в byte, проверка на преобразование без потери данных
+                        Console.WriteLine($"Здравствуйте, {name}, сколько Вам лет?");
+                        byte age = checked((byte)int.Parse(Console.ReadLine()));
+            // Запрос размера обуви, запись в переменную MyShoeSize 
+                        Console.WriteLine($"{name}, Какой у Вас размер обуви?");
+            double MyShoeSize = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"{name}, Какой у Вас любимый день недели (1-7)?");
+            DayOfWeek day = (DayOfWeek) int.Parse(Console.ReadLine());
+            //Запрос должности, запись в переменную work
             Console.WriteLine($"Хорошо, {name}, а как называется Ваша должность?");
-            var work = Console.ReadLine(); //Запрос должности, запись в переменную work
-            Console.WriteLine($"\t{name}, \n\tВам {age} \n\tВы работаете на должности {work}\nразмер обуви {MyShoeSize}");
+            var work = Console.ReadLine(); 
+            Console.WriteLine($"\t{name}, \n\tВам {age} \n\tВы работаете на должности {work}\n\tВаш любимый день недели {day}\n\tразмер обуви {MyShoeSize}");
         }
     }
 }
